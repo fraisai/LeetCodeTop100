e1142eddfc05c1d4361ba812856adae8b331f8f4
Raisa <9156403+fraisai@users.noreply.github.com>
2022-05-25 21:37:37 -0400

    Create strStr.js

---
+++ strStr.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+ 
+// 28. Implement strStr()
+// https://leetcode.com/problems/implement-strstr/
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+var strStr = function(haystack, needle) {
+    return index = haystack.indexOf(needle);
+};
